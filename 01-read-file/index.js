1298c32242d508fa65cc362fe5e4ee51082220b4
Alexey Gorbenko <a.v.gor@mail.ru>
2023-05-04 16:49:57 +0300

    feat: add decision 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,11 @@
+const fs = require('node:fs');
+const path = require('node:path'); 
 
+const pathToFile = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(pathToFile);
+
+let data = '';
+
+readStream.on('error', error => console.log('Error:', error.message));
+readStream.on('data', chunk => data += chunk);
+readStream.on('end', () => console.log(data));
