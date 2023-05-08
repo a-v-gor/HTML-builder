d34c131b2a76570781fca555deaee78ff50ad35c
Alexey Gorbenko <a.v.gor@mail.ru>
2023-05-08 12:21:05 +0300

    feat: add decision for task #04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+const fs = require('node:fs');
+const path = require('node:path');
 
+const srcDirPath = path.join(__dirname, 'files');
+const dstDirPath = path.join(__dirname, 'files-copy');
+
+function copyFile(file) {
+  if(!file.isFile()) {
+    return
+  }
+  const srcFilePath = path.join(srcDirPath, file.name);
+  const dstFilePath = path.join(dstDirPath, file.name);
+  fs.writeFile(dstFilePath, '',
+    (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  const input = fs.createReadStream(srcFilePath);
+  const output = fs.createWriteStream(dstFilePath);
+  input.pipe(output);
+}
+
+fs.rm(dstDirPath, { recursive: true, force: true }, (err) => {
+  if (err) {
+    throw err;
+  }
+  fs.mkdir(dstDirPath, () => {
+    fs.readdir(srcDirPath, { withFileTypes: true }, function (err, files) {
+      if (err) {
+        throw err;
+      }
+      files.forEach(copyFile);
+    });
+  });
+});
